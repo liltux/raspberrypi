9c6bc453e6fd00f1ab3bb63fc085fc6e77ac8727
liltux <jvanfleet@gmail.com>
2017-04-17 17:59:42 -0500

    Update GenMsgNotification.sh

--- GenMsg/GenMsgNotification.sh
+++ GenMsg/GenMsgNotification.sh
@@ -7,3 +7,11 @@
 sleep 10
 
 # Now a line to get information and filter through grep and email.
+# Only uncomment either wlan0 or eth0 depending if using wired or wireless
+# network.
+
+uptime > genmsgnotify
+who >> genmsgnotify
+ifconfig wlan0 | grep -2a wlan0 >> genmsgnotify
+#ifconfig eth0 | grep -2a eth0 >> genmsgnotify
+cat genmsgnotify | mail -s "genmsgnotify" account@domain
